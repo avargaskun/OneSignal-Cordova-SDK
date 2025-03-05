1167c2aa0b9ea65721a1646400ead0504c6d1480
Fadi George <fadii925@gmail.com>
2025-03-04 17:04:53 -0800

    update version

--- src/ios/OneSignalPush.m
+++ src/ios/OneSignalPush.m
@@ -119,7 +119,7 @@ void processNotificationClicked(OSNotificationClickEvent* event) {
 
 void initOneSignalObject(NSDictionary* launchOptions) {
     OneSignalWrapper.sdkType = @"cordova";
-    OneSignalWrapper.sdkVersion = @"050210";
+    OneSignalWrapper.sdkVersion = @"050211";
     [OneSignal initialize:nil withLaunchOptions:launchOptions];
     initialLaunchFired = true;
 }
