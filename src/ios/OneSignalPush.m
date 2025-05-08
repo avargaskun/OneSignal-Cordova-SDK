2ea78e8fdd7e73bf1cd50042b62073380cb4354b
Nan <nan@onesignal.com>
2025-05-08 16:22:29 -0700

    release commit 5.2.12

--- src/ios/OneSignalPush.m
+++ src/ios/OneSignalPush.m
@@ -119,7 +119,7 @@ void processNotificationClicked(OSNotificationClickEvent* event) {
 
 void initOneSignalObject(NSDictionary* launchOptions) {
     OneSignalWrapper.sdkType = @"cordova";
-    OneSignalWrapper.sdkVersion = @"050211";
+    OneSignalWrapper.sdkVersion = @"050212";
     [OneSignal initialize:nil withLaunchOptions:launchOptions];
 }
 
